1250a8623aeec668a54159763c34016b16670982
yusefa2 <165082483+yusefa2@users.noreply.github.com>
2024-09-15 22:00:30 -0500

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -9,25 +9,19 @@ Student ReadStudentRecFromStream(std::istream& is) {
   unsigned int uin;
   double gpa;
   
+  if (is.fail()) {
+    return Student{};
+  }
+  
   if (!(is >> first_name)) {
     return Student{};
   }
 
-  if (is.peek() != ' ') {
-    return Student{};
-  }
-
-  is.ignore();
-
-  if (!(is >> last_name) || last_name.empty() || (last_name.back() != ',')) {
+  if (!(is >> last_name) || (last_name.back() != ',')) {
     return Student{};
   }
 
   last_name.pop_back();
-
-  if(!(is >> comma) || (comma != ',')) {
-    return Student{};
-  }
 
   if (!(is >> uin)) {
     return Student{};
