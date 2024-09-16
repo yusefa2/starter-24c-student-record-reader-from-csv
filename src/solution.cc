047670496afc0ccfa6359a57459a0e2cf99abfff
yusefa2 <165082483+yusefa2@users.noreply.github.com>
2024-09-15 19:45:58 -0500

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -1,6 +1,40 @@
 #include "solution.hpp"
 
 Student ReadStudentRecFromStream(std::istream& is) {
-  // your implementation here...
+  char comma = ',';
+  char random;
+  
+  if (is.fail()) {
+    return Student{};
+  }
+  
+  if (!(is >> first_name >> last_name)) {
+    return Student{};
+  }
+
+  if (!(is >> comma)) {
+    return Student{};
+  }
+
+  if (!(is >> uin)) {
+    return Student{};
+  }
+
+  if (!(is >> comma)) {
+    return Student{};
+  }
+
+  if (!(is >> gpa)) {
+    return Student{};
+  }
+
+  if (!(is >> random)) {
+    return Student{};
+  }
+  
+  Student student;
+  student.name = first_name + ' ' + last_name;
+  student.uin = uin;
+  student.gpa = gpa;
   return Student{};
 }
